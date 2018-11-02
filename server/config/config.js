228369c4f2c5330869dd4fad18fd50b7547d2983
Gustavo <gustavo.reyes@enkings.cl>
2018-11-01 21:37:53 -0300

    rest server-usuario dos

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 
 } else {
-    urlDB = 'mongodb://cafe-user:gmrm8180%@ds235807.mlab.com:35807/cafe';
+    urlDB = 'mongodb://cafe-user:a123456@ds235807.mlab.com:35807/cafe';
 }
 
+//urlDB = 'mongodb://cafe-user:a123456@ds235807.mlab.com:35807/cafe';
 process.env.URLcafe = urlDB;
